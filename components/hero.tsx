5264c9df626019ed57a8505d5f5b597b6091a94f
lanzmoncis <lanzmoncis@gmail.com>
2024-05-30 21:29:12 +0100

    update hero css

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,16 +3,17 @@ import { NavLinks } from "./ui/nav-links";
 
 export const Hero = () => {
   return (
-    <header className="relative bg-black-light">
+    <header className="bg-black-light relative flex flex-col h-[600px]">
+      <div className="bg-[url('/images/home/mobile/image-header.jpg')] bg-cover bg-center bg-no-repeat w-full h-[600px] absolute"></div>
       <div className="py-[32px] border-b-[0.5px] border-gray-extraDark relative z-10 bg-black-light">
         <NavLinks />
       </div>
-      <div className="bg-[url('/images/home/mobile/image-header.jpg')] bg-cover bg-center bg-no-repeat h-[600px] w-full absolute -mt-20 flex flex-col justify-center items-center">
-        <div className="relative text-2xl text-white flex flex-col justify-between items-center max-w-[329px] mx-auto h-[290px]">
+      <div className="relative flex justify-center items-center grow">
+        <div className="flex flex-col justify-between items-center h-[290px] max-w-[329px]">
           <div className="uppercase text-custom-overline text-gray-extraDark text-center">
             new product
           </div>
-          <h1 className="text-[36px] uppercase font-bold leading-[40px] tracking-[1.29px] text-center">
+          <h1 className="text-[36px] uppercase text-white font-bold leading-[40px] tracking-[1.29px] text-center">
             xx99 mark ii headphones
           </h1>
           <p className="text-custom-base text-gray-extraDark text-center h-[75px]">

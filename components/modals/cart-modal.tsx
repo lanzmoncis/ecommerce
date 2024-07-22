91658f4c28f31f42fa85b7a7bf9a8d63487dd546
lanzmoncis <lanzmoncis@gmail.com>
2024-07-23 00:50:12 +0100

    fix cart modal position

--- components/modals/cart-modal.tsx
+++ components/modals/cart-modal.tsx
@@ -11,7 +11,7 @@ interface CartModalProps {
 export const CartModal: React.FC<CartModalProps> = ({ isOpen, onClose }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="bg-white flex flex-col justify-center items-center gap-6 py-10 h-[320px]">
+      <div className="flex flex-col justify-center items-center gap-6 py-10 h-full">
         <div className="text-gray-500 text-custom-md">Your cart is empty</div>
         <ShoppingCart size={48} />
       </div>
--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Dialog, DialogContent } from "@/components/ui/dialog";
+import * as Dialog from "@radix-ui/react-dialog";
+import { useState } from "react";
 
 interface ModalProps {
   isOpen: boolean;
@@ -9,6 +10,7 @@ interface ModalProps {
 }
 
 export const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
   const onChange = (open: boolean) => {
     if (!open) {
       onClose();
@@ -16,10 +18,16 @@ export const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   };
 
   return (
-    <Dialog open={isOpen} onOpenChange={onChange}>
-      <DialogContent>
-        <div>{children}</div>
-      </DialogContent>
-    </Dialog>
+    <>
+      <Dialog.Root open={isOpen} onOpenChange={onChange}>
+        <Dialog.Portal container={container}>
+          <Dialog.Overlay className="fixed inset-0 z-50 bg-black/40 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
+          <Dialog.Content className="absolute top-32 left-[50%] md:left-[73%] xl:left-[83%] translate-x-[-50%] z-50 grid w-full max-w-[327px] md:max-w-[377px] gap-4 border bg-white shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg">
+            <div>{children}</div>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+      <div ref={setContainer} />
+    </>
   );
 };
